c0a855dc35b1795ee365c4022d0e6ba02a44e979
VISTALL <vistall.valeriy@gmail.com>
2014-04-11 14:04:47 +0300

    working on stack frame

--- dotnet-debugger-impl/src/org/mustbe/consulo/dotnet/debugger/DotNetStackFrame.java
+++ dotnet-debugger-impl/src/org/mustbe/consulo/dotnet/debugger/DotNetStackFrame.java
@@ -1,14 +1,20 @@
 package org.mustbe.consulo.dotnet.debugger;
 
+import java.io.File;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.ColoredTextContainer;
 import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.xdebugger.XDebuggerUtil;
 import com.intellij.xdebugger.XSourcePosition;
 import com.intellij.xdebugger.frame.XCompositeNode;
 import com.intellij.xdebugger.frame.XStackFrame;
 import com.intellij.xdebugger.frame.XValueChildrenList;
+import mono.debugger.Location;
 import mono.debugger.MethodMirror;
 import mono.debugger.MethodParameterMirror;
 import mono.debugger.StackFrameMirror;
@@ -30,14 +36,44 @@ public class DotNetStackFrame extends XStackFrame
 	@Override
 	public XSourcePosition getSourcePosition()
 	{
-		return super.getSourcePosition();
+		String fileName = myFrame.location().sourcePath();
+		if(fileName == null)
+		{
+			return null;
+		}
+		VirtualFile fileByPath = LocalFileSystem.getInstance().findFileByPath(fileName);
+		if(fileByPath == null)
+		{
+			return null;
+		}
+		return XDebuggerUtil.getInstance().createPosition(fileByPath, myFrame.location().lineNumber());
 	}
 
 	@Override
 	public void customizePresentation(ColoredTextContainer component)
 	{
 		component.setIcon(AllIcons.Nodes.Method);
-		component.append(myFrame.location().method().name(), SimpleTextAttributes.REGULAR_ATTRIBUTES);
+
+		Location location = myFrame.location();
+
+		component.append(location.method().name() + "()", SimpleTextAttributes.REGULAR_ATTRIBUTES);
+
+		StringBuilder builder = new StringBuilder();
+		String fileName = location.sourcePath();
+		if(fileName != null)
+		{
+			builder.append(":");
+			builder.append(new File(fileName).getName());
+		}
+
+		builder.append(":");
+		builder.append(location.lineNumber());
+		builder.append(":");
+		builder.append(location.columnNumber());
+		builder.append(", ");
+		builder.append(location.method().declaringType().qualifiedName());
+
+		component.append(builder.toString(), SimpleTextAttributes.GRAY_ATTRIBUTES);
 	}
 
 	@Override
