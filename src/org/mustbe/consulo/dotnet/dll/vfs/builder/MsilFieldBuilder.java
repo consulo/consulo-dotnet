a337238e4071cb2ecfe6cd42ec58905887b9d2b0
VISTALL <vistall.valeriy@gmail.com>
2014-05-21 16:31:53 +0300

    field attributes

--- src/org/mustbe/consulo/dotnet/dll/vfs/builder/MsilFieldBuilder.java
+++ src/org/mustbe/consulo/dotnet/dll/vfs/builder/MsilFieldBuilder.java
@@ -18,23 +18,58 @@ package org.mustbe.consulo.dotnet.dll.vfs.builder;
 
 import org.mustbe.consulo.dotnet.dll.vfs.builder.block.LineStubBlock;
 import org.mustbe.consulo.dotnet.dll.vfs.builder.block.StubBlock;
+import org.mustbe.consulo.dotnet.dll.vfs.builder.util.XStubUtil;
 import edu.arizona.cs.mbel.mbel.Field;
 import edu.arizona.cs.mbel.mbel.TypeDef;
+import edu.arizona.cs.mbel.signature.FieldAttributes;
 
 /**
  * @author VISTALL
  * @since 21.05.14
  */
-public class MsilFieldBuilder extends MsilSharedBuilder
+public class MsilFieldBuilder extends MsilSharedBuilder implements FieldAttributes
 {
 	public static void processField(Field field, TypeDef typeDef, StubBlock block)
 	{
 		StringBuilder builder = new StringBuilder();
 		builder.append(".field ");
+		if(XStubUtil.isSet(field.getFlags(), FieldAccessMask, Public))
+		{
+			builder.append("public ");
+		}
+		else if(XStubUtil.isSet(field.getFlags(), FieldAccessMask, Assembly))
+		{
+			builder.append("internal ");
+		}
+		else if(XStubUtil.isSet(field.getFlags(), FieldAccessMask, Private))
+		{
+			builder.append("private ");
+		}
+
+		if(XStubUtil.isSet(field.getFlags(), Static))
+		{
+			builder.append("static ");
+		}
+
+		if(XStubUtil.isSet(field.getFlags(), Literal))
+		{
+			builder.append("literal ");
+		}
+
+		if(XStubUtil.isSet(field.getFlags(), InitOnly))
+		{
+			builder.append("initonly ");
+		}
+
+		if(XStubUtil.isSet(field.getFlags(), Static))
+		{
+			builder.append("static ");
+		}
+
 		typeToString(builder, field.getSignature().getType(), typeDef);
 		builder.append(" ");
 		builder.append(field.getName());
-		builder.append(";\n");
+		builder.append("\n");
 
 		block.getBlocks().add(new LineStubBlock(builder));
 	}
