ae721c6887cda3b41af29a41ad208158257696f3
VISTALL <vistall.valeriy@gmail.com>
2015-01-09 18:16:51 +0300

    if exit code is not 0 - add error

--- src/org/mustbe/consulo/dotnet/compiler/DotNetCompiler.java
+++ src/org/mustbe/consulo/dotnet/compiler/DotNetCompiler.java
@@ -157,7 +157,10 @@ public class DotNetCompiler implements TranslatingCompiler
 			{
 				compileContext.addMessage(CompilerMessageCategory.ERROR, s, null, -1, -1);
 			}
-			compileContext.addMessage(CompilerMessageCategory.INFORMATION, "Exit code: " + processOutput.getExitCode(), null, -1, -1);
+			if(processOutput.getExitCode() != 0)
+			{
+				compileContext.addMessage(CompilerMessageCategory.ERROR, "Exit code: " + processOutput.getExitCode(), null, -1, -1);
+			}
 		}
 		catch(Exception e)
 		{
