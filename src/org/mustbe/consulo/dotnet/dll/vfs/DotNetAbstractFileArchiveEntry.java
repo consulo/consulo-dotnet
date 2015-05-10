bbd356f47c2008b867707f35b686df3ff3eb1682
VISTALL <vistall.valeriy@gmail.com>
2015-05-10 21:57:24 +0300

    cleanup

--- src/org/mustbe/consulo/dotnet/dll/vfs/DotNetAbstractFileArchiveEntry.java
+++ src/org/mustbe/consulo/dotnet/dll/vfs/DotNetAbstractFileArchiveEntry.java
@@ -3,7 +3,6 @@ package org.mustbe.consulo.dotnet.dll.vfs;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.util.List;
