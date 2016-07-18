b1c5994d6ecb96a53ad00551def9ba1b7b1a3759
VISTALL <vistall.valeriy@gmail.com>
2016-07-18 18:48:48 +0300

    update library
    fixed #79

--- msil-psi-impl/src/org/mustbe/consulo/msil/lang/psi/impl/elementType/MsilFileStubElementType.java
+++ msil-psi-impl/src/org/mustbe/consulo/msil/lang/psi/impl/elementType/MsilFileStubElementType.java
@@ -76,7 +76,7 @@ public class MsilFileStubElementType extends IStubFileElementType<MsilFileStub>
 	@Override
 	public int getStubVersion()
 	{
-		int version = 68;
+		int version = 69;
 		for(MsilStubIndexer msilStubIndexer : MsilStubIndexer.EP_NAME.getExtensions())
 		{
 			version += msilStubIndexer.getVersion();
