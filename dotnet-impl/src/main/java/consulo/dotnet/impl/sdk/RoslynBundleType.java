ac459cf0aac171c21ffaa96f184f5a9e2eb5f90f
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 15:13:25 +0400

    fix compile

--- dotnet-impl/src/main/java/consulo/dotnet/impl/sdk/RoslynBundleType.java
+++ dotnet-impl/src/main/java/consulo/dotnet/impl/sdk/RoslynBundleType.java
@@ -17,6 +17,7 @@
 package consulo.dotnet.impl.sdk;
 
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.content.bundle.SdkType;
 import consulo.dotnet.icon.DotNetIconGroup;
 import consulo.logging.Logger;
@@ -25,9 +26,9 @@ import consulo.process.cmd.GeneralCommandLine;
 import consulo.process.util.CapturingProcessUtil;
 import consulo.process.util.ProcessOutput;
 import consulo.ui.image.Image;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 
 /**
@@ -35,72 +36,61 @@ import java.io.File;
  * @since 08.06.2015
  */
 @ExtensionImpl
-public class RoslynBundleType extends SdkType
-{
-	private static final Logger LOG = Logger.getInstance(RoslynBundleType.class);
+public class RoslynBundleType extends SdkType {
+    private static final Logger LOG = Logger.getInstance(RoslynBundleType.class);
 
-	@Nonnull
-	public static RoslynBundleType getInstance()
-	{
-		return EP_NAME.findExtensionOrFail(RoslynBundleType.class);
-	}
+    @Nonnull
+    public static RoslynBundleType getInstance() {
+        return Application.get().getExtensionPoint(RoslynBundleType.class).findExtensionOrFail(RoslynBundleType.class);
+    }
 
-	public RoslynBundleType()
-	{
-		super("ROSLYN_BUNDLE");
-	}
+    public RoslynBundleType() {
+        super("ROSLYN_BUNDLE");
+    }
 
-	@Override
-	public boolean isValidSdkHome(String path)
-	{
-		return new File(path, "csc.exe").exists();
-	}
+    @Override
+    public boolean isValidSdkHome(String path) {
+        return new File(path, "csc.exe").exists();
+    }
 
-	@Nullable
-	@Override
-	public String getVersionString(String sdkHome)
-	{
-		GeneralCommandLine commandLine = new GeneralCommandLine();
-		commandLine.setExePath(sdkHome + "/" + "csc.exe");
-		commandLine.addParameter("/version");
+    @Nullable
+    @Override
+    public String getVersionString(String sdkHome) {
+        GeneralCommandLine commandLine = new GeneralCommandLine();
+        commandLine.setExePath(sdkHome + "/" + "csc.exe");
+        commandLine.addParameter("/version");
 
-		try
-		{
-			ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(commandLine);
-			String version = processOutput.getStdout().trim();
-			// 3.0.1-dev (dev-build)
-			int i = version.indexOf("(");
-			if(i != -1)
-			{
-				version = version.substring(0, i);
-				version = version.trim();
-			}
-			return version;
-		}
-		catch(ExecutionException e)
-		{
-			LOG.error(e);
-		}
-		return "?";
-	}
+        try {
+            ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(commandLine);
+            String version = processOutput.getStdout().trim();
+            // 3.0.1-dev (dev-build)
+            int i = version.indexOf("(");
+            if (i != -1) {
+                version = version.substring(0, i);
+                version = version.trim();
+            }
+            return version;
+        }
+        catch (ExecutionException e) {
+            LOG.error(e);
+        }
+        return "?";
+    }
 
-	@Override
-	public String suggestSdkName(String currentSdkName, String sdkHome)
-	{
-		return getPresentableName() + " " + getVersionString(sdkHome);
-	}
+    @Override
+    public String suggestSdkName(String currentSdkName, String sdkHome) {
+        return getPresentableName() + " " + getVersionString(sdkHome);
+    }
 
-	@Nonnull
-	@Override
-	public String getPresentableName()
-	{
-		return "Roslyn";
-	}
+    @Nonnull
+    @Override
+    public String getPresentableName() {
+        return "Roslyn";
+    }
 
-	@Nullable
-	@Override
-	public Image getIcon()
-	{
-		return DotNetIconGroup.netfoundation();
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return DotNetIconGroup.netfoundation();
+    }
 }
