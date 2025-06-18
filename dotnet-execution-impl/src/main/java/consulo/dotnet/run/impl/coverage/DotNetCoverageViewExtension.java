9b35f6fd0f33f2999bab3e83a6fc1b1cb2384e7e
UNV <nikolay@yurchenko.su>
2025-06-18 18:52:58 +0300

    Reformatting DotNetCoverageViewExtension.

--- dotnet-execution-impl/src/main/java/consulo/dotnet/run/impl/coverage/DotNetCoverageViewExtension.java
+++ dotnet-execution-impl/src/main/java/consulo/dotnet/run/impl/coverage/DotNetCoverageViewExtension.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package consulo.dotnet.run.impl.coverage;
 
 import com.intellij.rt.coverage.data.ClassData;
@@ -37,6 +36,7 @@ import consulo.project.ui.view.tree.AbstractTreeNode;
 import consulo.ui.ex.awt.ColumnInfo;
 
 import jakarta.annotation.Nullable;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -46,158 +46,137 @@ import java.util.function.Supplier;
  * @author VISTALL
  * @since 10.01.15
  */
-public class DotNetCoverageViewExtension extends CoverageViewExtension
-{
-	private static final String UNKNOWN = "?? %";
+public class DotNetCoverageViewExtension extends CoverageViewExtension {
+    private static final String UNKNOWN = "?? %";
 
-	private final GlobalSearchScope mySearchScope;
+    private final GlobalSearchScope mySearchScope;
 
-	public DotNetCoverageViewExtension(Project project, CoverageSuitesBundle suitesBundle, CoverageViewManager.StateBean stateBean)
-	{
-		super(project, suitesBundle, stateBean);
-		mySearchScope = getSuitesBundle().getSearchScope(getProject());
-	}
+    public DotNetCoverageViewExtension(Project project, CoverageSuitesBundle suitesBundle, CoverageViewManager.StateBean stateBean) {
+        super(project, suitesBundle, stateBean);
+        mySearchScope = getSuitesBundle().getSearchScope(getProject());
+    }
 
-	@Nullable
-	@Override
-	public String getSummaryForNode(AbstractTreeNode node)
-	{
-		return null;
-	}
+    @Nullable
+    @Override
+    public String getSummaryForNode(AbstractTreeNode node) {
+        return null;
+    }
 
-	@Nullable
-	@Override
-	public String getSummaryForRootNode(AbstractTreeNode childNode)
-	{
-		return null;
-	}
+    @Nullable
+    @Override
+    public String getSummaryForRootNode(AbstractTreeNode childNode) {
+        return null;
+    }
 
-	@Nullable
-	@Override
-	public String getPercentage(int columnIdx, AbstractTreeNode node)
-	{
-		Object value = node.getValue();
-		if(!(value instanceof PsiElement))
-		{
-			return UNKNOWN;
-		}
-		double percentValue = getPercentValue((PsiElement) value, -1);
-		if(percentValue == -1)
-		{
-			return UNKNOWN;
-		}
-		else
-		{
-			return percentValue + " %";
-		}
-	}
+    @Nullable
+    @Override
+    public String getPercentage(int columnIdx, AbstractTreeNode node) {
+        Object value = node.getValue();
+        if (!(value instanceof PsiElement)) {
+            return UNKNOWN;
+        }
+        double percentValue = getPercentValue((PsiElement) value, -1);
+        if (percentValue == -1) {
+            return UNKNOWN;
+        }
+        else {
+            return percentValue + " %";
+        }
+    }
 
-	private double getPercentValue(PsiElement value, double unknownValue)
-	{
-		if(value instanceof DotNetTypeDeclaration)
-		{
-			String vmQName = ((DotNetTypeDeclaration) value).getVmQName();
-			if(vmQName == null)
-			{
-				return unknownValue;
-			}
+    private double getPercentValue(PsiElement value, double unknownValue) {
+        if (value instanceof DotNetTypeDeclaration) {
+            String vmQName = ((DotNetTypeDeclaration) value).getVmQName();
+            if (vmQName == null) {
+                return unknownValue;
+            }
 
-			ClassData classData = getSuitesBundle().getCoverageData().getOrCreateClassData(vmQName);
+            ClassData classData = getSuitesBundle().getCoverageData().getOrCreateClassData(vmQName);
 
-			LineData[] lines = (LineData[]) classData.getLines();
-			if(lines == null)
-			{
-				return unknownValue;
-			}
+            LineData[] lines = (LineData[]) classData.getLines();
+            if (lines == null) {
+                return unknownValue;
+            }
 
-			int len = 0;
-			double i = 0;
-			for(LineData line : lines)
-			{
-				if(line != null)
-				{
-					if(line.getHits() > 0)
-					{
-						i++;
-					}
-					len ++;
-				}
-			}
+            int len = 0;
+            double i = 0;
+            for (LineData line : lines) {
+                if (line != null) {
+                    if (line.getHits() > 0) {
+                        i++;
+                    }
+                    len++;
+                }
+            }
 
-			return (i / len) * 100;
-		}
-		else if(value instanceof DotNetNamespaceAsElement)
-		{
-			Collection<PsiElement> children = ((DotNetNamespaceAsElement) value).getChildren(mySearchScope, DotNetNamespaceAsElement.ChildrenFilter.NONE);
-			double all = 0;
-			for(PsiElement temp : children)
-			{
-				all += getPercentValue(temp, 0);
-			}
+            return (i / len) * 100;
+        }
+        else if (value instanceof DotNetNamespaceAsElement) {
+            Collection<PsiElement> children =
+                ((DotNetNamespaceAsElement) value).getChildren(mySearchScope, DotNetNamespaceAsElement.ChildrenFilter.NONE);
+            double all = 0;
+            for (PsiElement temp : children) {
+                all += getPercentValue(temp, 0);
+            }
 
-			return all / children.size();
-		}
-		return unknownValue;
-	}
+            return all / children.size();
+        }
+        return unknownValue;
+    }
 
-	@Override
-	public List<AbstractTreeNode> getChildrenNodes(final AbstractTreeNode node)
-	{
-		return ApplicationManager.getApplication().runReadAction(new Supplier<List<AbstractTreeNode>>()
-		{
-			@Override
-			public List<AbstractTreeNode> get()
-			{
-				List<AbstractTreeNode> nodes = new ArrayList<AbstractTreeNode>();
-				Object element = node.getValue();
-				if(element instanceof DotNetNamespaceAsElement)
-				{
-					Collection<PsiElement> children = ((DotNetNamespaceAsElement) element).getChildren(mySearchScope, DotNetNamespaceAsElement.ChildrenFilter.NONE);
-					for(PsiElement element1 : children)
-					{
-						if(element1 instanceof PsiNamedElement)
-						{
-							CoverageListRootNode e = new CoverageListRootNode(getProject(), (PsiNamedElement) element1, getSuitesBundle(),
-									getStateBean());
-							e.setParent(node);
-							nodes.add(e);
-						}
-					}
-				}
-				else if(element instanceof DotNetTypeDeclaration)
-				{
-					for(DotNetNamedElement element1 : ((DotNetTypeDeclaration) element).getMembers())
-					{
-						if(element1 instanceof DotNetTypeDeclaration)
-						{
-							CoverageListRootNode e = new CoverageListRootNode(getProject(), element1, getSuitesBundle(), getStateBean());
-							e.setParent(node);
-							nodes.add(e);
-						}
-					}
-				}
-				return nodes;
-			}
-		});
-	}
+    @Override
+    public List<AbstractTreeNode> getChildrenNodes(final AbstractTreeNode node) {
+        return ApplicationManager.getApplication().runReadAction(new Supplier<List<AbstractTreeNode>>() {
+            @Override
+            public List<AbstractTreeNode> get() {
+                List<AbstractTreeNode> nodes = new ArrayList<AbstractTreeNode>();
+                Object element = node.getValue();
+                if (element instanceof DotNetNamespaceAsElement) {
+                    Collection<PsiElement> children =
+                        ((DotNetNamespaceAsElement) element).getChildren(mySearchScope, DotNetNamespaceAsElement.ChildrenFilter.NONE);
+                    for (PsiElement element1 : children) {
+                        if (element1 instanceof PsiNamedElement) {
+                            CoverageListRootNode e = new CoverageListRootNode(getProject(), (PsiNamedElement) element1, getSuitesBundle(),
+                                getStateBean()
+                            );
+                            e.setParent(node);
+                            nodes.add(e);
+                        }
+                    }
+                }
+                else if (element instanceof DotNetTypeDeclaration) {
+                    for (DotNetNamedElement element1 : ((DotNetTypeDeclaration) element).getMembers()) {
+                        if (element1 instanceof DotNetTypeDeclaration) {
+                            CoverageListRootNode e = new CoverageListRootNode(getProject(), element1, getSuitesBundle(), getStateBean());
+                            e.setParent(node);
+                            nodes.add(e);
+                        }
+                    }
+                }
+                return nodes;
+            }
+        });
+    }
 
-	@Override
-	public ColumnInfo[] createColumnInfos()
-	{
-		return new ColumnInfo[]{new ElementColumnInfo(), new PercentageCoverageColumnInfo(1, "Statistics, %", getSuitesBundle(), getStateBean())};
-	}
+    @Override
+    public ColumnInfo[] createColumnInfos() {
+        return new ColumnInfo[]{new ElementColumnInfo(), new PercentageCoverageColumnInfo(
+            1,
+            "Statistics, %",
+            getSuitesBundle(),
+            getStateBean()
+        )};
+    }
 
-	@Nullable
-	@Override
-	public PsiElement getParentElement(PsiElement element)
-	{
-		return element.getParent();
-	}
+    @Nullable
+    @Override
+    public PsiElement getParentElement(PsiElement element) {
+        return element.getParent();
+    }
 
-	@Override
-	public AbstractTreeNode createRootNode()
-	{
-		DotNetNamespaceAsElement namespace = DotNetPsiSearcher.getInstance(getProject()).findNamespace("", mySearchScope);
-		return new CoverageListRootNode(getProject(), namespace, getSuitesBundle(), getStateBean());
-	}
+    @Override
+    public AbstractTreeNode createRootNode() {
+        DotNetNamespaceAsElement namespace = DotNetPsiSearcher.getInstance(getProject()).findNamespace("", mySearchScope);
+        return new CoverageListRootNode(getProject(), namespace, getSuitesBundle(), getStateBean());
+    }
 }
