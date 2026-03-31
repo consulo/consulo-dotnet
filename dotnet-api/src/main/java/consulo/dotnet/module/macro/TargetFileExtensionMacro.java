3624a5f566178098ea143af1314dc8941bf795a3
UNV <nikolay@yurchenko.su>
2026-03-31 22:34:27 +0300

    Localizing TargetFileExtensionMacro.getDescription.

--- dotnet-api/src/main/java/consulo/dotnet/module/macro/TargetFileExtensionMacro.java
+++ dotnet-api/src/main/java/consulo/dotnet/module/macro/TargetFileExtensionMacro.java
@@ -4,52 +4,45 @@ import consulo.annotation.component.ExtensionImpl;
 import consulo.dataContext.DataContext;
 import consulo.dotnet.module.extension.DotNetModuleExtension;
 import consulo.language.util.ModuleUtilCore;
+import consulo.localize.LocalizeValue;
 import consulo.module.Module;
 import consulo.pathMacro.Macro;
 import consulo.util.dataholder.Key;
 
 /**
  * @author VISTALL
- * @since 18.08.14
+ * @since 2014-08-18
  */
 @ExtensionImpl
-public class TargetFileExtensionMacro extends Macro
-{
-	public static final Key<Boolean> DEBUG_SYMBOLS = Key.create("debug.symbols");
+public class TargetFileExtensionMacro extends Macro {
+    public static final Key<Boolean> DEBUG_SYMBOLS = Key.create("debug.symbols");
 
-	@Override
-	public String getName()
-	{
-		return "TargetFileExt";
-	}
+    @Override
+    public String getName() {
+        return "TargetFileExt";
+    }
 
-	@Override
-	public String getDescription()
-	{
-		return "Target file extension";
-	}
+    @Override
+    public LocalizeValue getDescription() {
+        return LocalizeValue.localizeTODO("Target file extension");
+    }
 
-	@Override
-	public String expand(DataContext dataContext)
-	{
-		final Module module = dataContext.getData(Module.KEY);
-		if(module == null)
-		{
-			return null;
-		}
-		DotNetModuleExtension extension = ModuleUtilCore.getExtension(module, DotNetModuleExtension.class);
-		if(extension != null)
-		{
-			Boolean data = dataContext.getData(DEBUG_SYMBOLS);
-			if(data == Boolean.TRUE)
-			{
-				return extension.getDebugFileExtension();
-			}
-			else
-			{
-				return extension.getTarget().getExtension();
-			}
-		}
-		return null;
-	}
+    @Override
+    public String expand(DataContext dataContext) {
+        Module module = dataContext.getData(Module.KEY);
+        if (module == null) {
+            return null;
+        }
+        DotNetModuleExtension extension = ModuleUtilCore.getExtension(module, DotNetModuleExtension.class);
+        if (extension != null) {
+            Boolean data = dataContext.getData(DEBUG_SYMBOLS);
+            if (data == Boolean.TRUE) {
+                return extension.getDebugFileExtension();
+            }
+            else {
+                return extension.getTarget().getExtension();
+            }
+        }
+        return null;
+    }
 }
